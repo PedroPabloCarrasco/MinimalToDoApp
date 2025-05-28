3ce8ad58ecdee5ef337dc1e70a7b9c02c70f7db3
Pedro Pablo Carrasco <pedropablocarrascob@gmail.com>
2025-05-28 16:49:44 -0400

    agrega función de ocultar tareas completadas

--- data/screens/Home.js
+++ data/screens/Home.js
@@ -1,9 +1,29 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import TodoList from '../../components/TodoList';
-import { todosData } from '../todos';
+import { todosData } from '../todos'; // asegúrate que está bien exportado
 
 export default function Home() {
+  const [localData, setLocalData] = React.useState(
+    todosData.sort((a, b) => a.isCompleted - b.isCompleted)
+  );
+
+  const [isHidden, setisHidden] = React.useState(false);
+
+  //Función para ocultar elementos
+  const handleHidePress = () => {
+    if (isHidden) {
+        setisHidden(false)
+        setLocalData(todosData.sort((a, b) => a.isCompleted - b.isCompleted))
+        return;
+    }
+    setisHidden(!isHidden)
+    //Filtramos las tareas y regresamos las tareas que no estan completadas
+    setLocalData(localData.filter(todo => !todo.isCompleted)) 
+
+
+  }
+
   return (
     <View style={styles.container}>
       <Image
@@ -11,13 +31,18 @@ export default function Home() {
         style={styles.pic}
       />
 
-    <Text style={styles.title}>Hoy</Text>
-    <TodoList todosData={todosData} />
-    <Text style={styles.title}>Mañana</Text>
-    <TodoList todosData={todosData} />
+      <View style={{flexDirection: 'row', alignItems: 'center', justifyContent:'space-between'}}>
+        <Text style={styles.title}>Hoy</Text>
 
-
+      <TouchableOpacity onPress={handleHidePress}>
+        <Text style={{ color: '#3478F6' }}>{isHidden ? "Mostrar tareas completadas": "Ocultar tareas completadas"}</Text>
+      </TouchableOpacity>
+        </View>
+      <TodoList todosData={localData.filter(todo => todo.isToday)} />
       
+
+      <Text style={styles.title}>Mañana</Text>
+      <TodoList todosData={todosData.filter(todo => !todo.isToday)} />
     </View>
   );
 }
@@ -33,14 +58,13 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     marginBottom: 20,
-    borderRadius: 21,
-    alignSelf:'flex-end'
+    borderRadius: 25,
+    alignSelf: 'flex-end',
   },
-
   title: {
     fontSize: 34,
     fontWeight: 'bold',
     marginBottom: 35,
     marginTop: 10,
-  }
+  },
 });
